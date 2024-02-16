e0c9193417660522aea6be1202ff71842101dbbf
Apple <apple@Apples-MacBook-Pro.local>
2024-02-17 01:54:03 +0330

    message app is done.

---
+++ message/tests.py
@@ -0,0 +1,31 @@
+from django.test import SimpleTestCase
+
+from django.urls import reverse
+# Create your tests here.
+
+
+class MessageTestCase (SimpleTestCase) :
+
+
+    def test_url_exist_at_correct_loction(self):
+
+        response = self.client.get ( "/message/" )       
+        self.assertEqual( response.status_code , 200 )
+
+
+    def test_url_available_by_name(self):
+        response = self.client.get ( reverse( 'message' ) )
+        self.assertEqual( response.status_code , 200 )
+
+
+    def test_template_name(self):
+
+        response = self.client.get ( reverse('message') )
+        self.assertTemplateUsed( response , 'home.html' )
+
+
+    def test_template_content(self):
+
+        response = self.client.get ( '/message/' )
+        self.assertContains( response , '<h1>ری استارت تنها راه نجات</h1>' )
+
---
+++ message/views.py
@@ -0,0 +1,16 @@
+#from django.shortcuts import render
+
+from django.views.generic import TemplateView
+
+
+
+#def messageView(request):
+#    return render(request, 'home.html')
+
+
+class MessageView ( TemplateView ) :
+    template_name = 'home.html'
+    #def get ( self , request ) :
+    #    return render ( request , 'home.html' )
+    
+  
