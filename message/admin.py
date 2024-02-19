200602ac819ceb301d31251814bf13084c571003
Apple <apple@Apples-MacBook-Pro.local>
2024-02-19 05:44:42 +0330

    message board is ready.

--- message/admin.py
+++ message/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+
+from .models import Message
 # Register your models here.
+
+
+
+admin.site.register(Message)
+
+
--- message/views.py
+++ message/views.py
@@ -1,16 +1,21 @@
 #from django.shortcuts import render
 
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
 
+from django.views.generic import ListView
+from .models import Message
 
 
 #def messageView(request):
 #    return render(request, 'home.html')
 
 
-class MessageView ( TemplateView ) :
-    template_name = 'home.html'
+#class MessageView ( TemplateView ) :
+#    template_name = 'home.html'
     #def get ( self , request ) :
     #    return render ( request , 'home.html' )
     
-  
+class MessageView ( ListView ) :
+    
+    model = Message
+    template_name = 'home.html'
